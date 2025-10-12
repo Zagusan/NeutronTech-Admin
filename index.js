10bd11f26376b50dc6cc126959241d6ecbd72e41
Zagusan <santiagopina@protonmail.com>
2025-10-12 10:35:05 -0600

    Cambiar tipo de indentación

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import { getDatabase, ref, onValue, child, get  } from "https://www.gstatic.com/
 
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  apiKey: "AIzaSyDAVC59pzwEGB5CPCZfgrGUGoCKKcMWSSo",
-  authDomain: "neutrontech-cd0d4.firebaseapp.com",
-  databaseURL: "https://neutrontech-cd0d4-default-rtdb.firebaseio.com",
-  projectId: "neutrontech-cd0d4",
-  storageBucket: "neutrontech-cd0d4.firebasestorage.app",
-  messagingSenderId: "960029465371",
-  appId: "1:960029465371:web:03afae6e3f161f33d5d1be"
+	apiKey: "AIzaSyDAVC59pzwEGB5CPCZfgrGUGoCKKcMWSSo",
+	authDomain: "neutrontech-cd0d4.firebaseapp.com",
+	databaseURL: "https://neutrontech-cd0d4-default-rtdb.firebaseio.com",
+	projectId: "neutrontech-cd0d4",
+	storageBucket: "neutrontech-cd0d4.firebasestorage.app",
+	messagingSenderId: "960029465371",
+	appId: "1:960029465371:web:03afae6e3f161f33d5d1be"
 };
 
 // Initialize Firebase
@@ -20,16 +20,16 @@ const database = getDatabase(app);
 
 function updateTable(val)
 {
-    for (let key in val) 
+	for (let key in val) 
 	{
 		const data = val[key];
 		
 		
-    }
+  }
 }
 
 const productRef = ref(database, '/products');
 onValue(productRef, (snapshot) => {
-  const data = snapshot.val();
-  updateTable(data);
+	const data = snapshot.val();
+	updateTable(data);
 });
